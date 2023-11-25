6620381933d3272b8b621ea691f25586d656f3ae
jraviS2023 <jravi@andrew.cmu.edu>
2023-11-25 15:23:59 -0800

    made changes to API layer to get the userId

--- apiLayer/src/main/java/com/campushare/apiLayer/ApiLayerApplication.java
+++ apiLayer/src/main/java/com/campushare/apiLayer/ApiLayerApplication.java
@@ -2,8 +2,11 @@ package com.campushare.apiLayer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import com.campushare.apiLayer.config.WebConfig;
 
 @SpringBootApplication
+@Import(WebConfig.class)
 public class ApiLayerApplication {
 
 	public static void main(String[] args) {
--- apiLayer/src/main/java/com/campushare/apiLayer/controller/APIController.java
+++ apiLayer/src/main/java/com/campushare/apiLayer/controller/APIController.java
@@ -1,13 +1,13 @@
 package com.campushare.apiLayer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import com.campushare.apiLayer.model.LoginRequest;
 import com.campushare.apiLayer.model.User;
 import com.campushare.apiLayer.service.APIService;
@@ -23,29 +23,37 @@ public class APIController {
         return service.getUserByUserId(userId);
     }
 
-    @GetMapping("/users/{username}")
-    public User getUserByUserName(@PathVariable String username) {
-        return service.getUserByUsername(username);
-    }
+    // @GetMapping("/users/{username}")
+    // public User getUserByUserName(@PathVariable String username) {
+    // return service.getUserByUsername(username);
+    // }
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
         // Implement validation and error handling as needed
+        System.out.println(loginRequest);
+
         String username = loginRequest.getUsername();
         String password = loginRequest.getPassword();
 
         User authenticatedUser = service.authenticateUser(username, password);
+        System.out.println(authenticatedUser);
+
+        HttpHeaders responseHeaders = new HttpHeaders();
 
         if (authenticatedUser != null) {
             // Generate a JWT token
-            String token = Jwts.builder()
-                    .setSubject(username)
-                    .signWith(SignatureAlgorithm.HS512, "yourSecretKey") // replace with your secret key
-                    .compact();
-            return token;
+            // String token = Jwts.builder()
+            // .setSubject(authenticatedUser.getUserId()) // from username changed to
+            // authenticatedUser.getUserId()
+            // // because we need userId for other services
+            // .signWith(SignatureAlgorithm.HS512, "yourSecretKey") // replace with your
+            // secret key
+            // .compact();
+            return ResponseEntity.ok().headers(responseHeaders).body(authenticatedUser.getUserId());
         } else {
             // Handle authentication failure
-            return "Invalid credentials";
+            return ResponseEntity.ok().headers(responseHeaders).body(null);
         }
 
     }
